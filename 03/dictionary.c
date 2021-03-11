ddbcc70526192951b02b3572d8bd8574142395f3
dlefarth <david.lefarth@gmail.com>
2021-03-11 15:57:45 +0100

    improved 3

--- 03/dictionary.c
+++ 03/dictionary.c
@@ -8,27 +8,25 @@ typedef struct {
     int value;
 } dict_element;
 
-static dict_element dictionary[LENGTH];
+dict_element dictionary[LENGTH];
 
 int calc_hash(int number) {
     return number % LENGTH;
 }
 
 int insert(int value) {
-    int initialPosition = calc_hash(value);
-    if(dictionary[initialPosition].state == FREE || dictionary[initialPosition].state == DELETED) {
-        dictionary[initialPosition].value = value;
-        dictionary[initialPosition].state = USED;
+    int pos = calc_hash(value);
+    if(dictionary[pos].state == FREE || dictionary[pos].state == DELETED) {
+		dictionary[pos] = (dict_element) {USED, value};
         return 1;
     } else {
-        int pos = initialPosition;
-        while(++pos % LENGTH != initialPosition) {
+        int initialPos = pos;
+        while(++pos % LENGTH != initialPos) {
             if(dictionary[pos].state == USED && dictionary[pos].value == value) {
                 return 1;
             }
             if(dictionary[pos].state == FREE || dictionary[pos].state == DELETED) {
-                dictionary[pos].value = value;
-                dictionary[pos].state = USED;
+				dictionary[pos] = (dict_element) {USED, value};
                 return 1;
             }
         }
@@ -37,12 +35,12 @@ int insert(int value) {
 }
 
 int delete_from_dict(int value) {
-    int initialPos = calc_hash(value);
+    int pos = calc_hash(value);
 
-    if(dictionary[initialPos].state == USED && dictionary[initialPos].value == value) {
-        dictionary[initialPos].state = DELETED;
+    if(dictionary[pos].state == USED && dictionary[pos].value == value) {
+        dictionary[pos].state = DELETED;
     } else {
-        int pos = initialPos;
+        int initialPos = pos;
         while(++pos % LENGTH != initialPos) {
             if(dictionary[pos].state == USED && dictionary[pos].value == value) {
                 dictionary[pos].state = DELETED;
@@ -57,11 +55,11 @@ int delete_from_dict(int value) {
 }
 
 int member(int value) {
-    int initialPos = calc_hash(value);
-    if(dictionary[initialPos].state == USED && dictionary[initialPos].value == value) {
+    int pos = calc_hash(value);
+    if(dictionary[pos].state == USED && dictionary[pos].value == value) {
         return 1;
     } else {
-        int pos = initialPos;
+        int initialPos = pos;
         while(++pos % LENGTH != initialPos) {
             if(dictionary[initialPos].state == USED && dictionary[pos].value == value) {
                return 1;
@@ -73,3 +71,4 @@ int member(int value) {
         return 0;
     }
 }
+
